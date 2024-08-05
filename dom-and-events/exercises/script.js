f9b4ea984452d8c0910aec27e094e13a77dfb539
craigfili <craig.filipowicz@gmail.com>
2024-08-05 16:50:10 -0500

    Exercise complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston, we have liftoff!';
+    });
+
+    function abortFunc() {
+        let abortMission = window.confirm("Are you sure you want to abort the mission?");
+        if (abortMission){
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }
+    }
+    missionAbort.addEventListener('click', abortFunc);
+
+    missionAbort.addEventListener("mouseover", event => {
+        missionAbort.style.backgroundColor = "red";
+    })
+    missionAbort.addEventListener("mouseout", event => {
+        missionAbort.style.backgroundColor = "";
+    })
 }
 
 window.addEventListener("load", init);
